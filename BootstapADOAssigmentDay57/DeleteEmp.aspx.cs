e6a6aa511dca32e9afae132964ef3224b17dcd64
Prasad Deshmukh <prasad.deshmukh@waiin.com>
2022-06-04 19:02:24 +0530

    Add project files.

---
+++ BootstapADOAssigmentDay57/DeleteEmp.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data.SqlClient;
+using Web.Services;
+using Web.Services.Utilities;
+
+namespace BootstapADOAssigmentDay57
+{
+    public partial class DeleteEmp : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          
+        }
+        protected void ButtonDelete_OnClick(object sender, EventArgs e)
+        {
+            DeleteData();
+        }
+
+
+        private void DeleteData()
+        {
+            var idText = Request.QueryString["id"];
+            var id = int.Parse(idText);
+
+            var empService = new EmployeeServices();
+
+            try
+            {
+                empService.Delete(id);
+
+                LabelStatus.ShowStatusMessage("Employee record successfully deleted!");
+            }
+            catch (SqlException sqlException)
+            {
+                Console.WriteLine(sqlException);
+                LabelStatus.ShowStatusMessage("Failed to delete Employee record! Maybe a foreign key was found! Please contact database admin!");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                LabelStatus.ShowStatusMessage("Failed to delete Employee record!");
+            }
+        }
+    }
+}
---
+++ BootstapADOAssigmentDay57/DeleteJob.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Web.Services;
+using Web.Services.Utilities;
+
+namespace BootstapADOAssigmentDay57
+{
+    public partial class DeleteJob : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void ButtonDelete_OnClick(object sender, EventArgs e)
+        {
+            DeleteData();
+        }
+
+
+        private void DeleteData()
+        {
+            var idText = Request.QueryString["id"];
+            var id = int.Parse(idText);
+
+            var empService = new JobServices();
+
+            try
+            {
+                empService.Delete(id);
+
+                LabelStatus.ShowStatusMessage("job record successfully deleted!");
+            }
+            catch (SqlException sqlException)
+            {
+                Console.WriteLine(sqlException);
+                LabelStatus.ShowStatusMessage("Failed to delete job record! Maybe a foreign key was found! Please contact database admin!");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                LabelStatus.ShowStatusMessage("Failed to delete job record!");
+            }
+        }
+    }
+}
